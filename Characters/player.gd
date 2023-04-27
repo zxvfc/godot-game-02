f83b3548534b9fc900fc7b8544f4687d3147672d
zxvfc <velya.96@gmail.com>
2023-04-27 22:00:26 +0200

    Added test scene and character

---
+++ Characters/player.gd
@@ -0,0 +1,38 @@
+extends CharacterBody2D
+
+const JUMP_BTN = "jump"
+const LEFT_BTN = "left"
+const RIGHT_BTN = "right"
+
+@export var speed : float = 200.0
+@export var jump_velocity : float = -150.0
+@export var double_jump_velocity : float = -100
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var has_double_jumped : bool = false
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	else: 
+		has_double_jumped = false
+
+	# Handle Jump.
+	if Input.is_action_just_pressed(JUMP_BTN):
+		if is_on_floor():
+			velocity.y = jump_velocity
+		elif not has_double_jumped:
+			velocity.y = double_jump_velocity
+			has_double_jumped = true
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis(LEFT_BTN, RIGHT_BTN)
+	if direction:
+		velocity.x = direction * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+
+	move_and_slide()
