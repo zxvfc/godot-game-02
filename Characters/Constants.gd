8fd9568d4b7cdd2e4f71e458d27751bc7a6778c1
zxvfc <velya.96@gmail.com>
2023-05-15 20:07:50 +0200

    addded attack animation and state

--- Characters/Constants.gd
+++ Characters/Constants.gd
@@ -5,6 +5,8 @@ const LEFT_BTN = "left"
 const RIGHT_BTN = "right"
 const UP_BTN = "up"
 const DOWN_BTN = "down"
-const BLEND_POSITION_PATH = "parameters/Move/blend_position"
+const ATTACK_BTN = "attack"
+
+const BLEND_POSITION_PATH = "parameters/move/blend_position"
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
--- Characters/GroundState.gd
+++ Characters/GroundState.gd
@@ -4,7 +4,9 @@ class_name GroundState
 
 @export var jump_velocity: float = -150.0
 @export var air_state: State
-@export var jump_animation: String = "jump"
+@export var jump_animation_node: String = "jump"
+@export var attack_state: State
+@export var attack_animation_node: String = "attack"
 
 func state_process(delta):
 	if !character.is_on_floor():
@@ -13,8 +15,14 @@ func state_process(delta):
 func handle_input(event: InputEvent):
 	if event.is_action_pressed(Constants.JUMP_BTN):
 		jump()
+	if event.is_action_pressed(Constants.ATTACK_BTN):
+		attack()
 		
 func jump():
 	character.velocity.y = jump_velocity
 	next_state = air_state
-	playback.travel(jump_animation)
+	playback.travel(jump_animation_node)
+
+func attack():
+	next_state = attack_state
+	playback.travel(attack_animation_node)
